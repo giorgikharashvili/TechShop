6808eeff587af3b8eda4dbc517d48a7259155b33
giorgi <giorgi.kharashvili01@gmail.com>
2025-06-06 17:02:35 +0400

    Added primary constructor

--- TechShop.Application/Features/Auth/Login/LoginCommandHandler.cs
+++ TechShop.Application/Features/Auth/Login/LoginCommandHandler.cs
@@ -1,28 +1,12 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using MediatR;
-using Microsoft.Extensions.Configuration;
-using TechShop.Domain.Entities;
-using TechShop.Infrastructure.Repositories.Interfaces;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
+﻿using MediatR;
 using TechShop.Application.Services.Interfaces;
 
-namespace TechShop.Application.Features.Auth.Login
-{
-    public class LoginCommandHandler : IRequestHandler<LoginCommand, string>
+namespace TechShop.Application.Features.Auth.Login;
+    public class LoginCommandHandler(IAuthService _authService) : IRequestHandler<LoginCommand, string>
     {
-        private readonly IAuthService _authService;
-
-        public LoginCommandHandler(IAuthService authService)
-        {
-            _authService = authService;
-        }
-
         public async Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
         {
             return await _authService.AuthenticateAsync(request.Email, request.Password);
         }
     }
-}
+
